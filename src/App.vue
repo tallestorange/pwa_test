f7deea2b8aeb24caabf44e87ebb0111e3d596d6b
tallestorange <tallestorangeyuhei@gmail.com>
2019-03-06 12:42:10 +0900

    make UI

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,50 @@
 <template>
   <v-app>
-    <v-toolbar app>
+    <v-toolbar app color="indigo">
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span class="font-weight-light white--text">competitive programming</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <v-layout justify-center>
+      <v-flex xs7>
+        <div v-for="article in articles" style="margin: 5px;">
+          <v-card>
+            
+            <v-layout row>
+            <v-card-title primary-title>
+              <div>
+                <div class="headline font-weight-bold">{{article.title}}</div>
+                <!-- <span>Listen to your favorite artists and albums whenever and wherever, online and offline.</span> -->
+              </div>
+            </v-card-title>
+            <v-spacer></v-spacer>
+            <div style="margin: 5px;">{{article.score}}</div>
+            </v-layout>
+
+            <!-- <v-card-actions>
+              <v-btn flat dark>Listen now</v-btn>
+            </v-card-actions> -->
+          </v-card>
+        </div>
+      </v-flex>
+      </v-layout>
     </v-content>
+
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import articles from './articles.json'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   },
   data () {
     return {
-      //
+      articles: articles
     }
   }
 }
