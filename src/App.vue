50e44e8c745bd9f12c0b146d60c8f07c91b19081
tallestorange <tallestorangeyuhei@gmail.com>
2019-03-06 17:00:52 +0900

    add toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
   <v-app>
     <v-toolbar app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
         <span class="font-weight-light">competitive programming</span>
       </v-toolbar-title>
     </v-toolbar>
+
+    <v-navigation-drawer v-model="drawer" temporary absolute>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-list>
+        <!-- <template v-for="(item, index) in items">
+          <v-list-tile :href="item.href" :to="{name: item.href}" :key="index">
+            <v-list-tile-action>
+              <v-icon light v-html="item.icon"></v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title v-html="item.title"></v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template> -->
+      </v-list>
+    </v-navigation-drawer>
 
     <router-view />
 
@@ -18,6 +35,7 @@ export default {
   },
   data () {
     return {
+      drawer: false
     }
   }
 }
